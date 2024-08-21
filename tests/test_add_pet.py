26685f7111e5b4027f76289034d6d55d212cf0ac
davidbl994 <blazevic.david@hotmail.com>
2024-08-21 22:05:26 +0200

    Adding test function that checks the behaviour of adding a new pet but with a missing field

--- tests/test_add_pet.py
+++ tests/test_add_pet.py
@@ -7,5 +7,19 @@ def test_add_pet(pet_api):
         new_pet = json.load(file)
 
     response = pet_api.add_pet(new_pet)
+
+    print(f"Response status code: {response.status_code}")
     assert response.status_code == 200
     assert response.json()["name"] == "Rex"
+
+def test_add_pet_missing_field(pet_api):
+    pet_file = os.path.join(os.path.dirname(__file__), '../data/new_pet_missing_field.json')
+    with open(pet_file, 'r') as file:
+        pet_missing_field = json.load(file)
+    response = pet_api.add_pet(pet_missing_field)
+
+    print(f"Response status code: {response.status_code}")
+    print(f"Response content: {response.json()}")
+
+    assert response.status_code == 200, f"Expected 200, but got {response.status_code}"
+    assert response.json()["status"] == "xoxo", "Expected status to be 'xoxo"
