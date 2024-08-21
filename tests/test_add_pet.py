28e14422c5ccbef6f497bc721c750476bce52cd7
davidbl994 <blazevic.david@hotmail.com>
2024-08-21 21:43:42 +0200

    Refactoring test function - now it loads the json data at runtime - separating test data from test logic

--- tests/test_add_pet.py
+++ tests/test_add_pet.py
@@ -1,13 +1,11 @@
-import pytest
+import json
+import os
 
 def test_add_pet(pet_api):
-    new_pet = {
-        "id": 99999,
-        "name": "Rex",
-        "photoUrls": ["https://images.app.goo.gl/vZDewg4s8hBy1SPv5"],
-        "tags": [{"id": 2, "name": "sharp"}],
-        "status": "not available"
-    }
+    pet_file = os.path.join(os.path.dirname(__file__), '../data/new_pet.json')
+    with open(pet_file, 'r') as file:
+        new_pet = json.load(file)
+
     response = pet_api.add_pet(new_pet)
     assert response.status_code == 200
     assert response.json()["name"] == "Rex"
