4c1df6416beb99fe5c8298f1d8beac46454b72d7
davidbl994 <blazevic.david@hotmail.com>
2024-08-30 12:23:20 +0200

    Moving tests in seperate test files, adding logger to log the test output

--- tests/test_pet/test_add_pet/test_add_pet.py
+++ tests/test_pet/test_add_pet/test_add_pet.py
@@ -1,39 +1,23 @@
 import json
 import os
+from utils.logger import get_logger
+
+logger = get_logger()
 
 def test_add_pet(pet_api):
-    pet_file = os.path.join(os.path.dirname(__file__), '../data/new_pet.json')
+    logger.info("Starting test: Adding a new pet")
+
+    pet_file = os.path.join(os.path.dirname(__file__), '../../../data/new_pet.json')
     with open(pet_file, 'r') as file:
         new_pet = json.load(file)
+    logger.info(f"Loaded pet data from {pet_file}: {new_pet}")
 
     response = pet_api.add_pet(new_pet)
+    logger.info(f"Received response with status code: {response.status_code}")
 
-    print(f"Response status code: {response.status_code}")
-    assert response.status_code == 200
-    assert response.json()["name"] == "Rex"
+    assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
+    logger.info("Status code is 200 as expected")
 
-def test_add_pet_missing_field(pet_api):
-    pet_file = os.path.join(os.path.dirname(__file__), '../data/new_pet_missing_field.json')
-    with open(pet_file, 'r') as file:
-        pet_missing_field = json.load(file)
-    response = pet_api.add_pet(pet_missing_field)
-
-    print(f"Response status code: {response.status_code}")
-    print(f"Response content: {response.json()}")
-
-    assert response.status_code == 200, f"Expected 200, but got {response.status_code}"
-    assert response.json()["status"] == "xoxo", "Expected status to be 'xoxo"
-
-def test_add_duplicate_pet(pet_api):
-    pet_file = os.path.join(os.path.dirname(__file__), '../data/new_pet.json')
-    with open(pet_file, 'r') as file:
-        new_pet = json.load(file)
-
-    response = pet_api.add_pet(new_pet)
-    assert response.status_code == 200, f"Expected 200 OK, but got {response.status_code}"
-
-    duplicate_pet = pet_api.add_pet(new_pet)
-    assert duplicate_pet.status_code == 200, f"Expected 200 OK, but got {response.status_code}"
-
-    fetched_pet = pet_api.get_pet(new_pet["id"]).json()
-    assert fetched_pet["name"] == new_pet["name"], f"Expected name to be {new_pet['name']}, but got {fetched_pet['name']}"
+    pet_name = response.json().get("name")
+    assert pet_name == "Rex", f"Expected pet name to be 'Rex', got {pet_name}"
+    logger.info(f"Pet name is '{pet_name}' as expected")
